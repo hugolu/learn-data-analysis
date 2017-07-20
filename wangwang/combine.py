28ec65201d491cf776b1d2f8ca2fb7e351019fdb
Hugo Lu <printk@gmail.com>
2017-07-20 11:57:25 +0800

    add new data

--- wangwang/combine.py
+++ wangwang/combine.py
@@ -9,5 +9,5 @@ if path.isfile('all.csv'):
     os.remove('all.csv')
 
 data = pd.concat([pd.read_csv(f) for f in os.listdir('.') if f.endswith('.csv')])
-del data['Unnamed: 6']
-data.to_csv('all.csv')
+#del data['Unnamed: 6']
+data.to_csv('all.csv', header=False)
